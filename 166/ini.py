099db5af0f6c268ffc924d61908bdd918b48bb4a
David Colton <davidcolton@gmail.com>
2019-08-28 17:10:28 +0100

    Initialial working solutions for 2/3 of the Bite

---
+++ 166/ini.py
@@ -0,0 +1,36 @@
+import configparser
+import re
+
+
+class ToxIniParser:
+    def __init__(self, ini_file):
+        """Use configparser to load ini_file into self.config"""
+        self.parser = configparser.ConfigParser()
+        self.parser.read(ini_file)
+
+    @property
+    def number_of_sections(self):
+        """Return the number of sections in the ini file.
+           New to properties? -> https://pybit.es/property-decorator.html
+        """
+        return len(self.parser.sections())
+
+    @property
+    def environments(self):
+        """Return a list of environments
+           (= "envlist" attribute of [tox] section)"""
+        env_list = sorted(
+            [
+                env.strip()
+                for env in re.split(
+                    r"\s*,\s*|\n", self.parser.get("tox", "envlist").strip()
+                )
+                if env.strip() != ""
+            ]
+        )
+        return env_list
+
+    @property
+    def base_python_versions(self):
+        """Return a list of all basepython across the ini file"""
+        pass
