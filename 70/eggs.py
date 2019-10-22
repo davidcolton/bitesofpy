243a4a5347294b297c1909263c46fa6b60e8dce9
David Colton <davidcolton@gmail.com>
2019-10-22 13:17:19 +0100

    Initial working solution for Bite 70

---
+++ 70/eggs.py
@@ -0,0 +1,20 @@
+from random import choice
+
+COLORS = "red blue green yellow brown purple".split()
+
+
+class EggCreator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.count += 1
+        if self.count > self.limit:
+            raise StopIteration
+        color = choice(COLORS)
+        return f"{color} egg"
+
