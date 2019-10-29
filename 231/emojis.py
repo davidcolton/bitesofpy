6e06bc8665efd95af9b9b13932926added5f814c
David Colton <davidcolton@gmail.com>
2019-10-29 07:46:12 +0000

    Bite 231 without the emoji library

--- 231/emojis.py
+++ 231/emojis.py
@@ -11,7 +11,9 @@ def get_emoji_indices(text: str) -> List[int]:
     """Given a text return indices of emoji characters"""
     ## Create the function to extract the emojis
     # Find all the emoji's
-    emojis_list = map(lambda x: "".join(x.split()), emoji.UNICODE_EMOJI.keys())
-    r = re.compile("|".join(re.escape(p) for p in emojis_list))
-    return [idx for idx, e in enumerate(text) if e in r.findall(text)]
+    # Could use the emoji library either
+    #   emojis_list = map(lambda x: "".join(x.split()), emoji.UNICODE_EMOJI.keys())
+    #   r = re.compile("|".join(re.escape(p) for p in emojis_list))
+    #   return [idx for idx, e in enumerate(text) if e in r.findall(text)]
 
+    return [idx for idx, e in enumerate(text) if e in re.findall(r"[^\w\s,]", text)]
