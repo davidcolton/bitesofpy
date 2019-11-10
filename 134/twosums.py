bd63bb9e34b21a088021039cdec554b77b54faf9
David Colton <davidcolton@gmail.com>
2019-11-10 14:36:22 +0000

    Small improvement to solution for Bite 134

--- 134/twosums.py
+++ 134/twosums.py
@@ -15,11 +15,17 @@ def two_sums(numbers, target):
     """
     # Create a DefaultDict to hold a list of all pairs that match the target
     sum_of_two = defaultdict(list)
-    sorted_and_index = sorted([NumIdx(num, idx) for idx, num in enumerate(numbers)])
-    for num_pair in product(sorted_and_index, sorted_and_index):
+
+    # Create a list of NumIdx named tuples
+    num_and_index = [NumIdx(num, idx) for idx, num in enumerate(numbers)]
+
+    # Populate DefaultDict with pairs that match total
+    for num_pair in product(num_and_index, num_and_index):
         if num_pair[0].index != num_pair[1].index:
             total = num_pair[0].number + num_pair[1].number
             sum_of_two[total].append(NumIdxPair(num_pair[0], num_pair[1]))
+
+    # Get the pair that meet the target criteria
     try:
         target_pair = sorted(sum_of_two[target])[0]
         return (target_pair.num_01.index, target_pair.num_02.index)
