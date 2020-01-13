4474b5ff5649fa2a22e5df0d738a9205ffc53faa
David Colton <davidcolton@gmail.com>
2020-01-13 13:55:24 +0000

    Initial working solution for Bite 254

---
+++ 254/scoping.py
@@ -0,0 +1,11 @@
+num_hundreds = -1
+
+
+def sum_numbers(numbers: list) -> int:
+    """Sums passed in numbers returning the total, also
+       update the global variable num_hundreds with the amount
+       of times 100 fits in total"""
+    sum_of_numbers = sum(numbers)
+    global num_hundreds
+    num_hundreds += sum_of_numbers // 100
+    return sum_of_numbers
