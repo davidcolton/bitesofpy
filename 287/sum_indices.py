2a4fe9090b24511c9c5feb487f1d54eaaab71dd4
David Colton <davidcolton@gmail.com>
2020-08-08 00:05:52 +0100

    Optomised solution

--- 287/sum_indices.py
+++ 287/sum_indices.py
@@ -4,14 +4,9 @@ from collections import defaultdict
 
 
 def sum_indices(items: List[str]) -> int:
-    # Handle simple scenario separately
-    if len(items) <= 1:
-        return 0
-
     # Handle all other scenarios
     indices = defaultdict(list)
     for idx, char in enumerate(items):
         indices[char].append(idx)
-    cumulative_totals = []
-    return sum([sum(accumulate(values)) for values in indices.values()])
+    return sum(sum(accumulate(values)) for values in indices.values())
 
